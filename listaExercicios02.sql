9230f21ca37175f40211007c6f8de0e19563cedc
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-09-26 21:52:30 -0300

    Exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,17 @@ END //
 DELIMITER ; 
 -- Teste 
 CALL sp_ListarAutores();
+
+
+DELIMITER //
+-- 2. Livros por Categoria
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+-- Teste 
+CALL sp_LivrosPorCategoria('Ciência');
