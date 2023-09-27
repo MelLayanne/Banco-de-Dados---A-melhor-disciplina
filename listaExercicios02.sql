ba18d0f018a167dba5a598c7ec7be4d83703faef
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-09-26 21:53:27 -0300

    Exercício 3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -21,3 +21,18 @@ END //
 DELIMITER ;
 -- Teste 
 CALL sp_LivrosPorCategoria('Ciência');
+
+
+DELIMITER //
+-- 3. Contagem de Livros por Categoria
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100), OUT total INT)
+BEGIN
+    SELECT COUNT(*) INTO total
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+-- Teste 
+CALL sp_ContarLivrosPorCategoria('Autoajuda', @total);
+SELECT @total;
