a63633b146da5b59e1a54dc8a5a37b0c3135bdd9
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-09-26 21:58:19 -0300

    Exercício 6

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -70,3 +70,17 @@ END //
 DELIMITER ;
 -- Teste 
 CALL sp_LivrosAteAno(2007);
+
+
+DELIMITER //
+-- 6. Extração de Títulos por Categoria
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+-- Teste 
+CALL sp_TitulosPorCategoria('Romance');
