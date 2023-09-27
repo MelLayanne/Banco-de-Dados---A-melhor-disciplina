2b34e2d87f26044a9fe7c3509d4bbe661a60ecc9
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-09-26 21:54:53 -0300

    Exercício 4

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -36,3 +36,24 @@ DELIMITER ;
 -- Teste 
 CALL sp_ContarLivrosPorCategoria('Autoajuda', @total);
 SELECT @total;
+
+DELIMITER //
+-- 4. Verificação de Livros por Categoria
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoriaNome VARCHAR(100), OUT possuiLivros VARCHAR(3))
+BEGIN
+    DECLARE tt INT;
+    SELECT COUNT(*) INTO tt
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+
+    IF total > 0 THEN
+        SET possuilivro = 'Sim';
+    ELSE
+        SET possuilivro = 'Não';
+    END IF;
+END //
+DELIMITER ;
+-- Teste 
+CALL sp_VerificarLivrosCategoria('Romance', @possuilivro);
+SELECT @possuilivro;
